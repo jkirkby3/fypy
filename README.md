5050d379e6ade6e5bc540506a252d32a73b88775
jkirkby3 <jkirkby33@gmail.com>
2021-04-22 19:19:28 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,41 @@
+
+# FyPy
+
+
+
+## Coming Soon !
+
+- Exotic Option Pricing - Asian, Barrier, American, Parisian, Cliquet, Variance Swaps, etc.
+- Models: Stochastic Volatility, Regime Switching, Stochastic Local Vol
+- Levy Model Calibration (VG, NIG, CGMY, MJD, Kou, etc)
+- Regime Switching Calibration
+- Stochastic Volatility Calibration
+
+## User installation
+
+Coming soon, for now use git clone.
+
+## Dependencies
+
+
+fypy requires:
+
+- Python (>= 3.7)
+- NumPy (tested with 1.20.2)
+
+
+## Source code
+
+
+You can check the latest sources with the command
+
+    git clone https://github.com/jkirkby3/fypy.git
+    
+    
+
+## Example: Price Variance Gamma Model with PROJ (Fourier) Method
+
+```python
+
+
+```
---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+
+
+setup(name='fypy',
+      version='0.0.1',
+      description='Financial Quant model calibration and option pricing',
+      long_description='Financial Quant model calibration and option pricing',
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Scientific/Engineering ',
+        "Operating System :: OS Independent",
+      ],
+      keywords='quant option-pricing calibration fit levy stochastic-volatility',
+      url='https://github.com/jkirkby3/fypy.git',
+      author='Justin Lars Kirkby',
+      author_email='jkirkby33@gmail.com',
+      license='MIT',
+      packages=find_packages(where="fypy"),
+      python_requires=">=3.7",
+      install_requires=[
+          'numpy',
+      ],
+      include_package_data=True,
+      zip_safe=False)
