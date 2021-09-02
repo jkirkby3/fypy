b53574536ee708c4ef01dc45e68077f96d484ff4
jkirkby3 <jkirkby33@gmail.com>
2021-09-02 10:17:13 -0400

    Move targets on their own

--- fypy/fit/Objective.py
+++ fypy/fit/Objective.py
@@ -1,6 +1,5 @@
 from abc import ABC, abstractmethod
 import numpy as np
-from typing import Callable
 
 
 class Objective(ABC):
@@ -23,26 +22,3 @@ class Objective(ABC):
         raise NotImplementedError
 
 
-class Targets(Objective):
-    def __init__(self,
-                 weights: np.ndarray,
-                 targets: np.ndarray,
-                 function: Callable[..., np.ndarray],
-                 strength: float = 1.0):
-        """
-        An objective representing a set of targets (e.g. market prices, volatilities, etc)
-        :param weights: np.ndarray, the weight to apply per target
-        :param targets: np.ndarray, the targets themselves (e.g. market prices)
-        :param function: function, evaluated at each of the targets, determines the residual
-        :param strength: float, the strength of this particular objective
-        """
-        super().__init__(strength=strength)
-        # take sqrt since they are applied to residual before squaring.
-        # The strength is in space of sqrt(sum(squares)), so let it be squared with the resituals
-        self._weights = self._strength * np.sqrt(weights)
-        self._targets = targets
-        self._function = function
-
-    def value(self) -> np.ndarray:
-        """ Evaluate the targets objective, returns residuals per target """
-        return self._weights * (self._function() - self._targets)
---
+++ fypy/fit/Targets.py
@@ -0,0 +1,33 @@
+from typing import Callable, Optional
+
+import numpy as np
+
+from fypy.fit.Objective import Objective
+
+
+class Targets(Objective):
+    def __init__(self,
+                 targets: np.ndarray,
+                 function: Callable[..., np.ndarray],
+                 weights: Optional[np.ndarray] = None,
+                 strength: float = 1.0):
+        """
+        An objective representing a set of targets (e.g. market prices, volatilities, etc)
+        :param weights: np.ndarray, the weight to apply per target
+        :param targets: np.ndarray, the targets themselves (e.g. market prices)
+        :param function: function, evaluated at each of the targets, determines the residual
+        :param strength: float, the strength of this particular objective
+        """
+        super().__init__(strength=strength)
+        # take sqrt since they are applied to residual before squaring.
+        # The strength is in space of sqrt(sum(squares)), so let it be squared with the resituals
+        if weights is None:
+            weights = np.ones_like(targets) / len(targets)
+        self._weights = self._strength * np.sqrt(weights)
+        self._targets = targets
+        self._function = function
+
+    def value(self) -> np.ndarray:
+        """ Evaluate the targets objective, returns residuals per target """
+        return self._weights * (self._function() - self._targets)
+
