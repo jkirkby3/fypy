ccf8e5a1384a3bc56d29dc8b895398d541e5f2fc
jkirkby3 <jkirkby3@github.com>
2022-12-05 11:17:02 -0500

    Optimize the implied vol calc for black76 to avoid multiple calls to discount and fwd term structures

--- fypy/volatility/implied/ImpliedVolCalculator.py
+++ fypy/volatility/implied/ImpliedVolCalculator.py
@@ -71,9 +71,53 @@ class ImpliedVolCalculator_Black76(ImpliedVolCalculator):
                   is_call: bool,
                   ttm: float) -> float:
         """ Note: the underlying is forward for Black76 """
-        cp = 1 if is_call else -1
+        return self._imply_vol(price=p, strike=strike, is_call=is_call, ttm=ttm,
+                               disc=self._disc_curve(ttm),
+                               fwd=self._fwd_curve(ttm))
+
+    def imply_vols(self,
+                   strikes: np.ndarray,
+                   prices: np.ndarray,
+                   is_calls: Union[np.ndarray, List[int]],
+                   ttm: float) -> np.ndarray:
+        """
+        Calculates bid and ask implied vols from bid and ask prices, for a single time to maturity
+        Note: you can override this with a vectorized version if available
+        :param strikes: np.ndarray, strikes per option
+        :param prices: np.ndarray, prices per option (same order as strikes)
+        :param is_calls: np.ndarray of bools, true for call, false for put
+        :param ttm: float, Time to maturity (common to all options)
+        :return: np.ndarray of implied vols
+        """
+        vols = np.zeros_like(prices)
+
+        # This version precomputes the forward and discount for efficiency
         disc = self._disc_curve(ttm)
         fwd = self._fwd_curve(ttm)
+
+        # Compute Implied Vols
+        for i in range(len(vols)):
+            try:
+                vols[i] = self._imply_vol(price=prices[i],
+                                          strike=strikes[i],
+                                          is_call=is_calls[i],
+                                          ttm=ttm,
+                                          disc=disc,
+                                          fwd=fwd)
+            except Exception as e:
+                vols[i] = np.nan
+
+        return vols
+
+    def _imply_vol(self,
+                   price: float,
+                   strike: float,
+                   is_call: bool,
+                   ttm: float,
+                   disc: float,
+                   fwd: float
+                   ):
+        cp = 1 if is_call else -1
         return implied_volatility_from_a_transformed_rational_guess(price / disc, fwd, strike, ttm, cp)
 
 
