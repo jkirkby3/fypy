707635b8542a03d9124cea4fa5d87855c5e53073
ABHINAV AGRAWAL <agrawal.abhinav1696@gmail.com>
2023-09-02 15:33:50 -0400

    Update spread_option_kirk_method.py

--- fypy/pricing/analytical/spread_option_kirk_method.py
+++ fypy/pricing/analytical/spread_option_kirk_method.py
@@ -16,17 +16,18 @@ def spread_option_kirk_method(K : float,
                               q_1: float,
                               q_2: float) -> float:
     """
-    Params:
-    K: Option Strike
-    S0_1: Initial Price of asset 1
-    S0_2: Initial Price of asset 2
-    T: Time to maturity
-    r: Interest rate
-    rho: correlation coefficient b/w asset 1 and asset 2
-    sigma_1: Vol of asset 1
-    sigma_2: Vol of asset 2
-    q_1: Dividend on asset 1
-    q_2: Dividend on asset 2
+    Calculate price of 2D spread options
+    :param K: float, Option Strike
+    :param S0_1: float, Initial Price of asset 1
+    :param S0_2: float, Initial Price of asset 2
+    :param T: float, Time to maturity
+    :param r: float, Interest rate
+    :param rho: float, correlation coefficient b/w asset 1 and asset 2
+    :param sigma_1: float, Vol of asset 1
+    :param sigma_2: float, Vol of asset 2
+    :param q_1: float, Dividend on asset 1
+    :param q_2: float, Dividend on asset 2
+    :return float value of option price
     """
     f_1 = S0_1 * math.exp((r - q_1) * T)
     f_2 = S0_2 * math.exp((r - q_2) * T)
