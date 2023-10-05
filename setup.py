a08aa27ca7016d1a6a63a7577222defaa5e038d4
westonplatter <westonplatter+m22@gmail.com>
2023-10-04 20:50:29 -0600

    move depdencies into setup.py

--- setup.py
+++ setup.py
@@ -1,28 +1,50 @@
 from setuptools import setup, find_packages
 
-
-
-setup(name='fypy',
-      version='0.0.1',
-      description='Financial Quant model calibration and option pricing',
-      long_description='Financial Quant model calibration and option pricing',
-      classifiers=[
-        'Development Status :: 3 - Alpha',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.7',
-        'Topic :: Scientific/Engineering ',
-        "Operating System :: OS Independent",
-      ],
-      keywords='quant option-pricing calibration fit levy stochastic-volatility',
-      url='https://github.com/jkirkby3/fypy',
-      author='Justin Lars Kirkby',
-      author_email='jkirkby33@gmail.com',
-      license='MIT',
-      # packages=find_packages(where="fypy"),
-      packages=find_packages(),
-      python_requires=">=3.7",
-      install_requires=[
-          'numpy',
-      ],
-      include_package_data=True,
-      zip_safe=False)
+setup(
+  name='fypy',
+  version='0.0.1',
+  description='Financial Quant model calibration and option pricing',
+  long_description='Financial Quant model calibration and option pricing',
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.7',
+    'Topic :: Scientific/Engineering ',
+    "Operating System :: OS Independent",
+  ],
+  keywords='quant option-pricing calibration fit levy stochastic-volatility',
+  url='https://github.com/jkirkby3/fypy',
+  author='Justin Lars Kirkby',
+  author_email='jkirkby33@gmail.com',
+  license='MIT',
+  # packages=find_packages(where="fypy"),
+  packages=find_packages(),
+  python_requires=">=3.7",
+  install_requires=[
+    'numpy',
+  ],
+  dependencies=[
+    "cycler",
+    "kiwisolver",
+    "llvmlite==0.41.0",
+    "matplotlib",
+    "numpy",
+    "Pillow",
+    "pyparsing",
+    "python-dateutil",
+    "pytz",
+    "scipy",
+    "six",
+    "py_lets_be_rational>=1.0.1",
+    "pandas",
+    "setuptools",
+    "requests-cache",
+    "yfinance",
+    "seaborn",
+  ],
+  extras_require={
+    "test": []
+  ],
+  include_package_data=True,
+  zip_safe=False,
+)
