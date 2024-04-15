34c396960a7bd413da46a02a032fac5e8a42e92e
Justin Lars Kirkby <33753270+jkirkby3@users.noreply.github.com>
2024-04-15 19:41:27 -0400

    Update black_scholes.py
    Fix bug in delta

--- fypy/pricing/analytical/black_scholes.py
+++ fypy/pricing/analytical/black_scholes.py
@@ -97,7 +97,7 @@ def black76_delta(F: float,
     :return: float or np.ndarray, same shape as strikes
     """
     vol_st = vol * np.sqrt(T)
-    phi = 1 if is_call else 0
+    phi = 1 if is_call else -1
     d_1 = (np.log(F / K) + 0.5 * vol_st ** 2) / vol_st
     delta = phi * norm.cdf(phi * d_1)
     if is_fwd_delta:
