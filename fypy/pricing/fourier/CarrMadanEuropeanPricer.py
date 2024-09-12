d79e1350d24c26f090f40a1cb9ace5862fd6c7f2
GaetanoCodes <gaetano.agazzotti@hotmail.fr>
2024-09-12 13:32:45 +0200

    introducing carr-madan tempered stable

--- fypy/pricing/fourier/CarrMadanEuropeanPricer.py
+++ fypy/pricing/fourier/CarrMadanEuropeanPricer.py
@@ -3,35 +3,33 @@ from fypy.model.FourierModel import FourierModel
 import numpy as np
 from scipy.fft import fft
 from fypy.pricing.StrikesPricer import StrikesPricer
+from scipy.interpolate import interp1d as interp1
 
 
 class CarrMadanEuropeanPricer(StrikesPricer):
-    def __init__(self,
-                 model: FourierModel,
-                 alpha: float = 0.75,
-                 eta: float = 0.1,
-                 N: int = 2 ** 9):
+    def __init__(
+        self, model: FourierModel, alpha: float = 0.75, eta: float = 0.1, N: int = 2**9
+    ):
         self._model = model
         self._alpha = alpha  # contour shift param (see Lee for recommendations)
         self._eta = eta
         self._N = N
         self._logS0 = np.log(self._model.spot())
 
-    def price_strikes_fill(self,
-                           T: float,
-                           K: np.ndarray,
-                           is_calls: np.ndarray,
-                           output: np.ndarray):
+    def price(self, T: float, K: float, is_call: bool):
         lam = 2 * np.pi / (self._N * self._eta)
         b = self._N * lam / 2
 
         uv = np.arange(1, self._N + 1)  # TODO: check
         ku = -b + lam * (uv - 1)
         vj = (uv - 1) * self._eta
-        psij = self._chf(T=T, xi=vj - (self._alpha + 1) * 1j) \
-               / (self._alpha ** 2 + self._alpha - vj ** 2 + 1j * (2 * self._alpha + 1) * vj)
+
+        psij = self._chf(T=T, xi=vj - (self._alpha + 1) * 1j) / (
+            self._alpha**2 + self._alpha - vj**2 + 1j * (2 * self._alpha + 1) * vj
+        )
 
         disc = self._model.discountCurve(T)
+
         temp = (disc * self._eta / 3) * np.exp(1j * vj * b) * psij
         ind = np.zeros_like(uv)
         ind[0] = 1
@@ -40,10 +38,14 @@ class CarrMadanEuropeanPricer(StrikesPricer):
         Cku = np.real(np.exp(-self._alpha * ku) * fft(temp) / np.pi)
 
         logK = np.log(K)
-        # istrike = np.floor((logK + b) / lam + 1)
-        # xp = [ku(istrike) ku(istrike + 1)]
-        # yp = [Cku(istrike) Cku(istrike + 1)];
-        price = real(interp1(xp, yp, logK));
+        istrike = int(np.floor((logK + b) / lam + 1)) - 1
+
+        xp = [ku[istrike], ku[istrike + 1]]
+        yp = [Cku[istrike], Cku[istrike + 1]]
+        price = np.interp(logK, xp, yp)
+        if not is_call:
+            price = price - self._model.forwardCurve(T) * disc - K * disc
+        return price
 
     def _chf(self, T: float, xi: np.ndarray):
         return self._model.chf(T, xi) * np.exp(1j * self._logS0 * xi)
